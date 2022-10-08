ec220b699c809bd2d801dd8a2607e0793ad4483d
danillyamkin <danilwebdev@gmail.com>
2022-10-08 23:54:52 +0300

    Подключен куки парсер

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   // create app
@@ -16,6 +17,9 @@ async function bootstrap() {
 
   // validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // cookies
+  app.use(cookieParser());
 
   // get port
   const port = config.get<number>('API_PORT') || 3333;
