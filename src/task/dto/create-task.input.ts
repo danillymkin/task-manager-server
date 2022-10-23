3bf3f5b8b8759313630c1ebe2e6e93534504fa95
danillyamkin <danilwebdev@gmail.com>
2022-10-23 19:29:04 +0300

    Создан input type для создания новой задачи

--- src/task/dto/create-task.input.ts
+++ src/task/dto/create-task.input.ts
@@ -1,7 +1,26 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsDateString, IsIn, IsOptional, IsString } from 'class-validator';
 
 @InputType()
 export class CreateTaskInput {
+  @IsString({ message: 'Название задачи должно быть строкой' })
   @Field({ nullable: false })
-  name: string;
+  name!: string;
+
+  @IsOptional()
+  @IsString({ message: 'Описание задачи должно быть строкой' })
+  @Field({ nullable: true })
+  description?: string;
+
+  @IsOptional()
+  @IsIn([1, 2, 3, 4], { message: 'Приоритет должен быть числом от 1 до 4' })
+  @Field({ nullable: true })
+  priority?: number;
+
+  @IsOptional()
+  @IsDateString(
+    {},
+    { message: 'Срок выполнения должен быть строкой в формате ISO 8601' },
+  )
+  deadline?: string;
 }
