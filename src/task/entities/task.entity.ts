2dd668ac82ae22fceb8cb6023f3677c27ec9ea2b
danillyamkin <danilwebdev@gmail.com>
2022-10-23 19:53:30 +0300

    Описан класс Task

--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,7 +1,32 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { GraphQLDateTime } from 'graphql-scalars';
 
 @ObjectType()
 export class Task {
   @Field(() => ID, { nullable: false })
   id!: number;
+
+  @Field({ nullable: false })
+  name!: string;
+
+  @Field({ nullable: true })
+  description?: string;
+
+  @Field({ nullable: false })
+  priority!: number;
+
+  @Field(() => GraphQLDateTime, { nullable: true })
+  deadline?: string | Date;
+
+  @Field(() => GraphQLDateTime, { nullable: true })
+  completedAt?: string | Date;
+
+  @Field({ nullable: false })
+  isDeleted!: boolean;
+
+  @Field(() => GraphQLDateTime, { nullable: false })
+  createdAt: string | Date;
+
+  @Field(() => GraphQLDateTime, { nullable: false })
+  updatedAt: string | Date;
 }
